ffaec1fb0481a3e5ca0e54b4dd3d5b5071c88c00
juniperphoton <dengweichao@hotmail.com>
2023-02-08 17:47:58 +0800

    Initial Commit

---
+++ SwiftUISheetIssue/SwiftUISheetIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISheetIssueApp.swift
+//  SwiftUISheetIssue
+//
+//  Created by Photon Juniper on 2023/2/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISheetIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
